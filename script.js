75edf5bf1f4bae1612141bf29b7f9f4cc4bb801c
Zecorr <zecorr88@gmail.com>
2023-04-07 17:00:24 -0300

    Handle user clicks with sound

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 `use strict`;
+
+//Global Variables
+const buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let started = false;
+let level = 0;
+
+//User Clicks
+$(".btn").click(function () {
+  const userChosenColor = $(this).attr("id");
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+});
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+// Play Sound
+function playSound(name) {
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
